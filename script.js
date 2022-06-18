08847b6a57a3f1bfcb258c2a54937ee406cd8aaf
sameoldefi <105250912+sameoldefi@users.noreply.github.com>
2022-06-18 03:35:38 +0300

    Full working sketch

---
+++ script.js
@@ -0,0 +1,59 @@
+let color = 'black';
+let click = true;
+
+function populateBoard(size) {
+  let board = document.querySelector('.board');
+  let squares = board.querySelectorAll('div');
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement('div');
+    square.addEventListener('mouseover', colorSquare);
+    square.style.backgroundColor = 'white';
+    board.insertAdjacentElement("beforeend", square);
+  }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
+    populateBoard(input);
+  } else {
+    console.log("Too many squares");
+  }
+}
+
+function colorSquare() {
+  if (click) {
+    if ((color === `random`)) {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
+    }
+  }
+}
+
+function changeColor(choise) {
+  color = choise;
+}
+function resetBoard() {
+  let board = document.querySelector('.board');
+  let squares = board.querySelectorAll('div');
+  squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+
+
+document.querySelector('body').addEventListener('click', () => {
+  click = !click;
+  if (click) {
+    document.querySelector('.mode').textContent = "Mode: Coloring"
+  } else {
+    document.querySelector('.mode').textContent = "Mode: Not Coloring"
+
+  }
+});
